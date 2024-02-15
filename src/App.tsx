dd5e07b16600f8d6ee96014802b120c9e22f83d6
WwLuo-1024 <abrahamwong1408@gmail.com>
2024-02-15 20:36:10 +0000

    chore: landing page initialization

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/**
+ * @description App.tsx
+ * @author Luo Wang
+ */
+import React from "react";
+import { ConfigProvider, App as AntdApp } from "antd";
+import "./App.less";
+import LandingPage from "./views/LandingPage/LandingPage";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: "#473BF0",
+        },
+      }}
+    >
+      <AntdApp>
+        <LandingPage />
+      </AntdApp>
+    </ConfigProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ src/views/LandingPage/LandingPage.tsx
@@ -0,0 +1,12 @@
+/**
+ * @description Landing Page
+ * @author Luo Wang
+ */
+
+import React, { FC } from "react";
+
+const LandingPage: FC = () => {
+  return <div>Landing Page</div>;
+};
+
+export default LandingPage;
