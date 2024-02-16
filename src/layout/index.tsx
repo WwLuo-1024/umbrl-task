4efa019bfa737a1252d0448a56079d9e04f68034
WwLuo-1024 <abrahamwong1408@gmail.com>
2024-02-16 14:53:16 +0000

    feat: layout creation

---
+++ src/layout/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Layout } from "antd";
+import { Outlet } from "react-router-dom";
+import NavHeader from "@/components/NavHeader";
+import NavFooter from "@/components/NavFooter/inded";
+import styles from "./index.module.less";
+
+const { Content } = Layout;
+
+const App: React.FC = () => {
+  return (
+    <Layout>
+      <NavHeader />
+      <Content className={styles.content}>
+        <div>
+          <Outlet />
+        </div>
+      </Content>
+      <NavFooter />
+    </Layout>
+  );
+};
+
+export default App;
